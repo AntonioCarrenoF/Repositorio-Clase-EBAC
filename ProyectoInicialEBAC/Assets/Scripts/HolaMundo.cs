2a0888d31568a2b09d328752528f7ef5e28f59cc
AntonioCarrenoF <antonio234@icloud.com>
2024-06-18 00:31:11 -0600

    Funciones Void
    -Se crearon componentes 1,2 y 3.
    -Se utilizaron las funciones Void Awake, Start, Update.

--- ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("Algo paso");
-        Debug.Log("Algo Paso");
         Debug.LogWarning("Algo Salio Medianamente Mal");
         Debug.LogError("Algo Salio Muy Mal");
     }
@@ -16,6 +19,29 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola Desde Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola Desde Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola Desde Late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El Objeto Ha Sido Habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("El Objeto Ha Sido Inhabilitado");
     }
 }
