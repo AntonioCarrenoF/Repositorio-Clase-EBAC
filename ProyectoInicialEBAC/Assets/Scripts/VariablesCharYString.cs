4c51dc3fac6799572019db0fc5c5077645777bd2
AntonioCarrenoF <antonio234@icloud.com>
2024-06-28 23:15:53 -0600

    Modificación script VariablesCharYString
    -Se agregaron ejemplos de variablesString y se observó su comportamiento

--- ProyectoInicialEBAC/Assets/Scripts/VariablesCharYString.cs
+++ ProyectoInicialEBAC/Assets/Scripts/VariablesCharYString.cs
@@ -9,11 +9,27 @@ public class VariablesCharYString : MonoBehaviour
     {
         char c = 'k';
         int ValorEntero = 0;
-        if (!int.TryParse(c.ToString(),out ValorEntero))
+        if (!int.TryParse(c.ToString(), out ValorEntero))
         {
             Debug.LogError("eso no es un tipo de dato valido");
         }
         Debug.Log(ValorEntero);
+
+        char MiCaracter;
+        string MiString = "Hola desde EBAC";
+        string MiSegundoString = MiString.ToUpper();
+        string MiTercerString = MiString + " " + MiSegundoString;
+        string ejemploEscape = "c:\\user\nAntonio\\Documentos";
+        MiCaracter = MiString[13];
+        string miNombre = "Antonio";
+        string misApellidos = "Carreño Flores";
+        string miPrimerApellido = misApellidos.Substring(0, 7);
+        string salidaSuma = "Mi nombre es " + miNombre + " y mis apellidos son " + misApellidos;
+        string salida = $"Mi nombre es {miNombre} y mis apellidos son {misApellidos}";
+        int Longitud = MiString.Length;
+        Debug.Log(salida);
+        Debug.Log(miPrimerApellido);
+
     }
 
     // Update is called once per frame
